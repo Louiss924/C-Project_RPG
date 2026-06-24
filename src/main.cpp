cbc7698fe0f4264c21e8d7c52798ba22081b9227
Louiss924 <a0900553262@gmail.com>
2026-06-24 18:15:44 +0800

    feat: resize console window to 95 cols and 35 lines on startup

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 int main() {
     // 強制呼叫 system("chcp 65001 > nul") 解決 Windows 控制台中文與圖形字元亂碼
     system("chcp 65001 > nul");
+    // 設定控制台視窗大小為 95 欄寬、35 列高，以完美適配卡牌與戰鬥介面
+    system("mode con: cols=95 lines=35");
     
     Game game;
     game.run();
